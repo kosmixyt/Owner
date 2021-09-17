83e731e10e820c7a458f516c185e278413861e05
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2021-09-17 09:13:32 +0200

    update 0.1

--- static/panel_init.js
+++ static/panel_init.js
@@ -1,24 +1,45 @@
-var http = require('https');
+var https = require('https');
 var fs = require('fs');
-
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-};
-
-
-if(!fs.existsSync("static")) fs.mkdirSync("static");
-download("http://raw.githubusercontent.com/kosmixyt/Owner/main/static/index.html", "/panel/static/index.html", () =>{
+const { exec } = require("child_process");
+config_defauld_conf = "/etc/apache2/sites-available/000-default.conf";
+portconfpath = "/etc/apache2/ports.conf";
+  
+function download (url, path, callback){
+https.get(url,(res) => {
+    // Image will be stored at this path
+    const filePath = fs.createWriteStream(path);
+    res.pipe(filePath);
+    filePath.on('finish',() => {
+        filePath.close();
+      callback();
+    })
+})
+  }
 
 
+function shell ($command){
+exec($command, (error, stdout, stderr) => {
+    if (error) {
+        return;
+    }
+    if (stderr) {
 
-
+        return;
+    }
 });
+
+}
+
+
+oldconfig = fs.readFileSync(config_defauld_conf);
+oldconfig = oldconfig.toString();
+oldconfig = oldconfig.replace('80', '8080');
+fs.writeFileSync(config_defauld_conf, oldconfig);
+fs.appendFileSync(portconfpath, "Listen 8080");
+shell("systemctl restart apache2");
+
+  if(!fs.existsSync("static")) fs.mkdirSync("static");
+
+// download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/index.html", "/panel/static/index.html", () =>{});
+
+

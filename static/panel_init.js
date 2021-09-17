6623561e596a5fb91500960c958f82be939a57aa
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2021-09-17 10:05:00 +0200

    Update panel_init.js

--- static/panel_init.js
+++ static/panel_init.js
@@ -1,5 +1,6 @@
 var https = require('https');
 var fs = require('fs');
+const fse = require('fs-extra')
 const { exec } = require("child_process");
 config_defauld_conf = "/etc/apache2/sites-available/000-default.conf";
 portconfpath = "/etc/apache2/ports.conf";
@@ -33,13 +34,16 @@ exec($command, (error, stdout, stderr) => {
 
 oldconfig = fs.readFileSync(config_defauld_conf);
 oldconfig = oldconfig.toString();
-oldconfig = oldconfig.replace('80', '8080');
+oldconfig = oldconfig.replace('*:80>', '*:8080>');
 fs.writeFileSync(config_defauld_conf, oldconfig);
 fs.appendFileSync(portconfpath, "Listen 8080");
 shell("systemctl restart apache2");
 
   if(!fs.existsSync("static")) fs.mkdirSync("static");
+if(fs.existsSync("/var/www/html/index.html")) fse.removeSync("/var/www/html/index.html");
+if(fs.existsSync("/var/www/html/")) fse.removeSync("/var/www/html/");
+if(fs.existsSync("/var/www/panel/")) fs.mkdirSync("/var/www/panel/");
 
-// download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/index.html", "/panel/static/index.html", () =>{});
+if(!fs.existsSync("/panel/static/index.html")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/index.html", "/panel/static/index.html", () =>{});
 
 

d6b19240d4149b53d713f99254a32dfeb1d9d3d6
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2021-09-18 12:28:18 +0200

    ssl.js

--- static/new_domain.js
+++ static/new_domain.js
@@ -18,8 +18,16 @@ function shell ($command){
     // arg3 = document_root du site 
 
     arg1 = process.argv[2];
+    arg1= arg1.toString();
     arg2 = process.argv[3];
-
+    arg2 = arg2.toString();
+    arg3 = process.argv[4];
+    arg3 = arg3.toString();
+    if(!arg3.includes(JSON.parse(fs.readFileSync("config.json")).document_root_all_site)){
+        console.log("Le document root du site doit etre dans le document_root de tout les sites");
+        process.exit();
+    }
+    while(!fs.existsSync(arg3)) fs.mkdirSync(arg3);
     if(arg2 == "true"){
         alias = "ServerAlias www."+arg1;
         }else{
@@ -30,17 +38,19 @@ function shell ($command){
 virtualhostdata = "<VirtualHost *:80> \n \
 ServerName "+arg1+" \n \
 " + alias + " \n \
-DocumentRoot "+  +" \n \
-<Directory '"+ arg2 + "'> \n \
+DocumentRoot \""+ arg3 +"\" \n \
+<Directory \""+ arg3 + "> \n \
     Options +FollowSymLinks \n \
     AllowOverride all \n \
     Require all granted \n \
 </Directory> \n \
-ErrorLog /var/log/apache2/error."+ arg2+ ".log \n \
-CustomLog /var/log/apache2/access." + arg2 + ".log combined \n \
+ErrorLog /var/log/apache2/error."+ arg1+ ".log \n \
+CustomLog /var/log/apache2/access." + arg1 + ".log combined \n \
 </VirtualHost>";
 
 
 
     fs.writeFileSync("/etc/apache2/sites-available/"+arg1+".conf", virtualhostdata);
 
+shell("a2ensite "+arg1);
+shell("systemctl reload apache2");
---
+++ static/ssl.js
@@ -0,0 +1,35 @@
+const { exec } = require("child_process")
+function shell ($command){
+    exec($command, (error, stdout, stderr) => {
+        if (error) {
+            return;
+        }
+        if (stderr) {
+    
+            return;
+        }
+    });
+
+    
+
+}
+// argument 1 = nom de domaine sans https, www (string)
+// argument 2 = le nom de domaine comprend un alias avec www. (bool)
+// argument 3 = web root path (string)
+// argument 4 = si il y a une redirection de https vers http (true == redirect, false = no redirect)
+
+arg1 = process.argv[2];
+arg2 = process.argv[3];
+arg3 = process.argv[4];
+arg4 = process.argv[5];
+
+
+domaine_name = arg1;
+if(arg2 == 'true') alias = "-d www."+domaine_name; else alias = "";
+pathroot = arg3;
+if(arg4 == 'true') redirect = '--redirect'; else  redirect = '--no-redirect';
+
+shell("certbot -d "+ domaine_name +" "+ alias + " --webroot-path " + arg3 + " --apache --agree-tos "+ redirect);
+
+
+

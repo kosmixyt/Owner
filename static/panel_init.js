1c261e5c891b126a083159d3c9a2cf0cf23a0fed
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2021-09-19 20:08:46 +0200

    Update panel_init.js

--- static/panel_init.js
+++ static/panel_init.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 const fse = require('fs-extra')
 const { exec } = require("child_process");
 const passGen = require("fast-pass-gen");
+const bcrypt = require('bcrypt');
+var unzipper = require("unzipper");
 config_defauld_conf = "/etc/apache2/sites-available/000-default.conf";
 portconfpath = "/etc/apache2/ports.conf";
   
@@ -74,26 +76,49 @@ if(!fs.existsSync("/panel/node")) fs.mkdirSync("/panel/node");
 if(!fs.existsSync("/panel/static/index.html")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/index.html", "/panel/static/index.html", () =>{});
 if(!fs.existsSync("/panel/node/install.js")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/install.js", "/panel/node/install.js", () =>{});
 if(!fs.existsSync("/panel/node/new_domain.js")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/new_domain.js", "/panel/node/new_domain.js", () =>{});
-if(!fs.existsSync("/panel/static/ip.html")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/ip.html", "/panel/static/ip.html", () =>{
+if(!fs.existsSync("/panel/static/ip.html")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/ip.html", "/panel/static/ip.html", () =>{});
 if(!fs.existsSync("/panel/node/ssl.js")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/ssl.js", "/panel/node/ssl.js", () =>{});
 if(!fs.existsSync("/panel/static/")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/ssl.js", "/panel/node/ssl.js", () =>{});
-if(!fs.existsSync("/var/www/panel/phpmyadmin/")) {
-  
-  download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/phpmyadmins.zip", "/var/www/panel/phpmyadmin.zip", () =>{});
-fs.createReadStream("/var/www/panel/phpmyadmin.zip").pipe(unzipper.Extract({ path: '/var/www/panel/phpmyadmin' }));
-}
 if(!fs.existsSync("/var/www/ip/index.html")) fs.copyFileSync("/panel/static/ip.html", "/var/www/ip/index.html");
-
-
-
-
-
+if(!fs.existsSync("/panel/static/")) download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/ssl.js", "/panel/node/ssl.js", () =>{});
+if(!fs.existsSync("/var/www/panel/phpmyadmin/")) 
+{
+  download("https://raw.githubusercontent.com/kosmixyt/Owner/main/static/phpmyadmins.zip", "/var/www/panel/phpmyadmin.zip", () =>{
+fs.createReadStream('/var/www/panel/phpmyadmin.zip').pipe(unzipper.Extract({ path: '/var/www/panel/' })).on('finish', () => {
+  console.log("fini");
+fs.renameSync("/var/www/panel/phpMyAdmin-5.1.1-all-languages/", "/var/www/panel/phpmyadmin/");
+fs.unlinkSync("/var/www/panel/phpmyadmin.zip")
 });
+  });
+}
+
+
+
+
+
+
 
 
 // gen mysql password
 mysqlpass = passGen(15, ["num", "eng"])
-console.log(mysqlpass)
+adminpanelpassword = passGen(15, ["num", "eng"])
+date = Date.now();
 
-// shell("mysql --execute=\"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '"+mysqlpass+"';\"")
-console.log("mysql -uroot -py2pzt4y6zwo8n7q --execute=\"CREATE DATABASE [IF NOT EXISTS] panel\"");
+
+console.log("Mysql 'root' password : "+mysqlpass)
+console.log("Panel 'ADMIN' password : "+ adminpanelpassword)
+
+//shell("mysql --execute=\"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '"+mysqlpass+"';\"");
+const saltRounds = 10;
+
+
+
+
+shell("mysql -uroot -" + mysqlpass+ " --execute=\"CREATE DATABASE panel;\"");
+shell("mysql -uroot -" + mysqlpass + " --execute='USE panel; CREATE TABLE `users` (`id` int NOT NULL,`username` varchar(50) NOT NULL,`last_login_ip` varchar(30) NOT NULL,`register_ip` varchar(30) NOT NULL,`user_type` varchar(10) NOT NULL,`register_date` varchar(300) NOT NULL,`password` varchar(300) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; ALTER TABLE `users` ADD PRIMARY KEY (`id`); ALTER TABLE `users` MODIFY `id` int NOT NULL AUTO_INCREMENT;COMMIT;'");
+
+bcrypt.hash(adminpanelpassword, saltRounds, function(err, hash) {
+ 
+
+shell("mysql -uroot -" + mysqlpass + " --execute='USE panel; INSERT INTO `users` (`id`, `username`, `last_login_ip`, `register_ip`, `user_type`, `register_date`, `password`) VALUES (NULL, \"admin\", \"0.0.0.0\", \"0.0.0.0\", \"admin_all\", \""+ date +"\", \"" + hash + "\")'");
+});
